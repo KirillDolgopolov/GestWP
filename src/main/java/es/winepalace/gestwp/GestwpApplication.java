dbc25eb12a16dc53b398c7b0ab4b536be8fc3ca6
Kirill Dolgopolov <kirdolgopolov@gmail.com>
2024-03-02 09:08:28 +0100

    start commit

---
+++ src/main/java/es/winepalace/gestwp/GestwpApplication.java
@@ -0,0 +1,13 @@
+package es.winepalace.gestwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestwpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/winepalace/gestwp/GestwpApplicationTests.java
@@ -0,0 +1,13 @@
+package es.winepalace.gestwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestwpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
